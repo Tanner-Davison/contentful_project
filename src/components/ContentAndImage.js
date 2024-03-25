56db283336288a6a932ba24deaee1075f9e25a78
Tanner Davison <Tboydavison@gmail.com>
2024-03-24 21:13:46 -0600

    added

--- src/components/ContentAndImage.js
+++ src/components/ContentAndImage.js
@@ -1,53 +1,53 @@
-import React from "react"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import styled from "styled-components"
-import media from "styles/media"
-import colors from "styles/colors"
-import text from "styles/text"
+// import React from "react"
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+// import styled from "styled-components"
+// import media from "styles/media"
+// import colors from "styles/colors"
+// import text from "styles/text"
 
-const ContentfulPost = ({ data }) => {
-  // const siteTitle = data?.site?.siteMetadata?.title || `Title`
-  // const { contentfulPage } = data
-  // const content =
-  //   contentfulPage?.body?.raw && JSON.parse(contentfulPage?.body?.raw, null, 2)
+// const ContentfulPost = ({ data }) => {
+//   const siteTitle = data?.site?.siteMetadata?.title || `Title`
+//   const { contentfulPage } = data
+//   const content =
+//     contentfulPage?.body?.raw && JSON.parse(contentfulPage?.body?.raw, null, 2)
 
-  // const text = {
-  //   eyebrow: contentfulPage?.eyebrow,
-  //   header: contentfulPage?.header,
-  //   headline: contentfulPage?.bodyHeader,
-  //   color: contentfulPage?.colors,
-  //   body: documentToReactComponents(content),
-  // }
-  // const image = {
-  //   url: contentfulPage?.heroImage?.url,
-  //   alt: contentfulPage?.heroImage?.alt,
-  //   orientation: contentfulPage?.imageOrientation,
-  // }
+//   const text = {
+//     eyebrow: contentfulPage?.eyebrow,
+//     header: contentfulPage?.header,
+//     headline: contentfulPage?.bodyHeader,
+//     color: contentfulPage?.colors,
+//     body: documentToReactComponents(content),
+//   }
+//   const image = {
+//     url: contentfulPage?.heroImage?.url,
+//     alt: contentfulPage?.heroImage?.alt,
+//     orientation: contentfulPage?.imageOrientation,
+//   }
 
-  return (
-    <Layout>
-      {/* <Wrapper>
-        {text.header && <Header>{text.header}</Header>}
-        <ImageAndContent>
-          <BodyWrapper>
-            {text.headline && (
-              <BodyHeadline $color={text.color}>{text.headline}</BodyHeadline>
-            )}
-            {text.body && <Body>{text?.body}</Body>}
-          </BodyWrapper>
-          {image.url && (
-            <HeroImage
-              src={image.url}
-              alt={image.alt}
-              $orientation={image.orientation}
-            />
-          )}
-        </ImageAndContent>
-      </Wrapper> */}
-    </Layout>
-  )
-}
-export default ContentfulPost
+//   return (
+//     <Layout>
+//       <Wrapper>
+//         {text.header && <Header>{text.header}</Header>}
+//         <ImageAndContent>
+//           <BodyWrapper>
+//             {text.headline && (
+//               <BodyHeadline $color={text.color}>{text.headline}</BodyHeadline>
+//             )}
+//             {text.body && <Body>{text?.body}</Body>}
+//           </BodyWrapper>
+//           {image.url && (
+//             <HeroImage
+//               src={image.url}
+//               alt={image.alt}
+//               $orientation={image.orientation}
+//             />
+//           )}
+//         </ImageAndContent>
+//       </Wrapper>
+//     </Layout>
+//   )
+// }
+// export default ContentfulPost
 // const Header = styled.h1`
 //   ${text.h1}
 //   text-align:center;
@@ -86,5 +86,22 @@ export default ContentfulPost
 //   display: flex;
 //   flex-direction: column;
 //   align-items: center;
-`
-
+// `
+// export const query = graphql`
+//   query ($slug: String!) {
+//     contentfulPage(slug: { eq: $slug }) {
+//       id
+//       body {
+//         raw
+//       }
+//       header
+//       heroImage {
+//         url
+//         description
+//       }
+//       imageOrientation
+//       bodyHeader
+//       colors
+//     }
+//   }
+// `
