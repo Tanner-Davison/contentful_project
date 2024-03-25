b0fdf05f5edd509ce622d67482a23e37e22d763b
Tanner Davison <Tboydavison@gmail.com>
2024-03-24 20:47:34 -0600

    commented out whole component

--- src/components/ContentAndImage.js
+++ src/components/ContentAndImage.js
@@ -6,27 +6,27 @@ import colors from "styles/colors"
 import text from "styles/text"
 
 const ContentfulPost = ({ data }) => {
-  const siteTitle = data?.site?.siteMetadata?.title || `Title`
-  const { contentfulPage } = data
-  const content =
-    contentfulPage?.body?.raw && JSON.parse(contentfulPage?.body?.raw, null, 2)
+  // const siteTitle = data?.site?.siteMetadata?.title || `Title`
+  // const { contentfulPage } = data
+  // const content =
+  //   contentfulPage?.body?.raw && JSON.parse(contentfulPage?.body?.raw, null, 2)
 
-  const text = {
-    eyebrow: contentfulPage?.eyebrow,
-    header: contentfulPage?.header,
-    headline: contentfulPage?.bodyHeader,
-    color: contentfulPage?.colors,
-    body: documentToReactComponents(content),
-  }
-  const image = {
-    url: contentfulPage?.heroImage?.url,
-    alt: contentfulPage?.heroImage?.alt,
-    orientation: contentfulPage?.imageOrientation,
-  }
+  // const text = {
+  //   eyebrow: contentfulPage?.eyebrow,
+  //   header: contentfulPage?.header,
+  //   headline: contentfulPage?.bodyHeader,
+  //   color: contentfulPage?.colors,
+  //   body: documentToReactComponents(content),
+  // }
+  // const image = {
+  //   url: contentfulPage?.heroImage?.url,
+  //   alt: contentfulPage?.heroImage?.alt,
+  //   orientation: contentfulPage?.imageOrientation,
+  // }
 
   return (
     <Layout>
-      <Wrapper>
+      {/* <Wrapper>
         {text.header && <Header>{text.header}</Header>}
         <ImageAndContent>
           <BodyWrapper>
@@ -43,48 +43,48 @@ const ContentfulPost = ({ data }) => {
             />
           )}
         </ImageAndContent>
-      </Wrapper>
+      </Wrapper> */}
     </Layout>
   )
 }
 export default ContentfulPost
-const Header = styled.h1`
-  ${text.h1}
-  text-align:center;
-  max-width: 60vw;
-`
-const HeroImage = styled.img.attrs(props => ({
-  width: props.$orientation ? "27.778vw" : "40%",
-}))`
-  height: auto;
-`
-const BodyHeadline = styled.p`
-  ${text.bodyXLBold}
-  color: ${props => (props.$color ? colors[props.$color] : "inherit")};
-  margin: unset;
-`
-const Body = styled.p`
-  ${text.bodyM};
-  max-width: 34.722vw;
-  b {
-    font-weight: bolder;
-    font-size: larger;
-  }
-`
-const BodyWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-const ImageAndContent = styled.div`
-  position: relative;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 5.083vw;
-`
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+// const Header = styled.h1`
+//   ${text.h1}
+//   text-align:center;
+//   max-width: 60vw;
+// `
+// const HeroImage = styled.img.attrs(props => ({
+//   width: props.$orientation ? "27.778vw" : "40%",
+// }))`
+//   height: auto;
+// `
+// const BodyHeadline = styled.p`
+//   ${text.bodyXLBold}
+//   color: ${props => (props.$color ? colors[props.$color] : "inherit")};
+//   margin: unset;
+// `
+// const Body = styled.p`
+//   ${text.bodyM};
+//   max-width: 34.722vw;
+//   b {
+//     font-weight: bolder;
+//     font-size: larger;
+//   }
+// `
+// const BodyWrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `
+// const ImageAndContent = styled.div`
+//   position: relative;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   gap: 5.083vw;
+// `
+// const Wrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
 `
 
