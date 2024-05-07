803ee2501ba22d019e87f9feda2673cb24ca518c
Tanner Davison <Tboydavison@gmail.com>
2024-05-06 21:29:09 -0600

    added iframs as page viewer

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,42 +3,74 @@ import { Link, graphql } from "gatsby"
 import {useEffect} from 'react'
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import styled from 'styled-components';
+import media from 'styles/media';
+import colors from 'styles/colors';
+import text from 'styles/text';
 
 const HomeIndex = ({ data }) => {
   const posts = data?.allContentfulPage?.edges
 
   return (
     <Layout>
-      <ol style={{ listStyle: `none` }}>
+      <PageListWrapper style={{ listStyle: `none` }}>
         {posts.map(post => {
           const title = post?.node?.title || post?.node?.slug
 
           return (
-            <li key={post?.node?.slug}>
-              <article
-                className="post-list-item"
-                itemScope
-                itemType="http://schema.org/Article"
-              >
-                <header>
-                  <h2>
-                    <Link to={post?.node?.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h2>
-                </header>
-              </article>
-            </li>
+            <PageItem key={post?.node?.slug}>
+                <Wrapper>
+                    <Linked to={post?.node?.slug} itemProp="url">
+                      <IFRAME className={'iframeBox'}src={title}itemProp="headline" scrolling='no'>{title}</IFRAME>
+                    </Linked>
+                </Wrapper>
+        
+            </PageItem>
           )
         })}
-      </ol>
+      </PageListWrapper>
     </Layout>
   )
 }
 
 export default HomeIndex
 
+const IFRAME = styled.iframe`
+width:100%;
+height: 100%;
+border:unset;
+border-radius: 25px;
+pointer-events: none;
+border:2px inset silver;
+
+`
+const Linked = styled(Link)`
+display: flex;
+align-items: center;
+justify-content: center;
+height: 400px;
+`
+const PageItem = styled.div`
+display: flex;
+align-items: center;
+justify-content: center;
+`
+const PageListWrapper = styled.div`
+display: flex;
+flex-direction: row;
+align-items: center;
+justify-content: space-between;
+
+`
+const Wrapper = styled.div`
+display: flex;
+width: 100%;
+height: auto;
+flex-wrap: wrap;
+padding:0vw 2vw;
+`
 export const Head = () => <Seo title="All posts" />
+
 
 export const pageQuery = graphql`
   query {
