fdb42189b9b14821ed1237cf10ca18c7c98dcd6a
Tanner Davison <Tboydavison@gmail.com>
2024-07-10 23:20:58 -0600

    deleted console log

--- src/components/SimpleCentered.js
+++ src/components/SimpleCentered.js
@@ -4,8 +4,6 @@ import media from "styles/media"
 import colors from "styles/colors"
 import text from "styles/text"
 const SimpleCentered = ({ content }) => {
-  console.log(content)
-
   return (
     <Wrapper>
       {content?.headline && <Headline>{content.headline}</Headline>}
@@ -17,11 +15,10 @@ export default SimpleCentered
 const Headline = styled.h2`
   ${text.h2}
   text-align: center;
-  
+
   ${media.mobile} {
     ${text.h2Mobile}
   }
-  
 `
 const Wrapper = styled.div`
   display: flex;
